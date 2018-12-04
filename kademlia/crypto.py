602effeba032b0fb7b213404fb9a31743efec878
Alex Agitolyev <alex_a_cybervision@eqitii.com>
2018-12-04 12:33:49 +0200

    enhance: use compact signatures

--- kademlia/crypto.py
+++ kademlia/crypto.py
@@ -7,19 +7,19 @@ log = logging.getLogger(__name__)
 
 class Crypto(object):
 
+    #TODO: add support of not compact signatures
     @staticmethod
     def get_signature(message: bytes, priv_key: str):
         priv_key = PrivateKey(priv_key, raw=False)
 
         sig = priv_key.ecdsa_sign(message)
-        return priv_key.ecdsa_serialize(sig)
+        return priv_key.ecdsa_serialize_compact(sig)
 
     @staticmethod
     def check_signature(message: bytes, signature: str, pub_key: str):
-        privkey = PrivateKey()
         pub_key = PublicKey(bytes(bytearray.fromhex(pub_key)), raw=True)
 
-        sig = privkey.ecdsa_deserialize(bytes(bytearray.fromhex(signature)))
+        sig = pub_key.ecdsa_deserialize_compact(bytes(bytearray.fromhex(signature)))
 
         return pub_key.ecdsa_verify(message, sig)
 
